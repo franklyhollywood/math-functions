e4b572f39c5d89f317145317e7d36a1debafd21b
Frank Hollywood <github@srvsync.com>
2021-08-19 13:34:10 -0700

    Passed multiply test with function

--- src/math-functions.js
+++ src/math-functions.js
@@ -16,7 +16,7 @@ export function sum(a, b) {
 	let sum = a + b;
 	answer.push(sum);
 	answer.push(`The sum of ${a} and ${b} is ${sum}.`);
-	return result;
+	return answer;
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
@@ -29,7 +29,13 @@ Write a function called multiply() that takes in two numbers as arguments and re
 "The product of 5 and 9 is 45."
 */
 
-export function multiply(a, b) {}
+export function multiply(a, b) {
+	let productAnswer = [];
+	let product = a * b;
+	productAnswer.push(product);
+	productAnswer.push(`The product of ${a} and ${b} is ${product}.`);
+	return productAnswer;
+}
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 // Don't forget to create a new branch for your work on the next question!
